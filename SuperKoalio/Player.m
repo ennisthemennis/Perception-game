1b2dd1e1df15d87e5f0ed0ce9c6cf13c66228cf1
ennisthemennis <mennis@mit.edu>
2014-01-29 23:54:54 -0500

    ?

--- SuperKoalio/Player.m
+++ SuperKoalio/Player.m
@@ -21,20 +21,20 @@
         self.velocity = ccp(250.0, 0.0);
         self.enemiesKilled = 0;
         
-       
+        
     }
     return self;
 }
 -(CGRect)collisionBoundingBox {
-   
- 
+    
+    
     CGRect collisionBox = CGRectInset(self.boundingBox, 3, 0);
     CGPoint diff = ccpSub(self.desiredPosition, self.position);
     CGRect returnBoundingBox = CGRectOffset(collisionBox, diff.x, diff.y);
     return returnBoundingBox;
 }
 -(void)update:(ccTime)dt {
-   
+    
     CGPoint gravity = ccp(0.0, -450.0);
     CGPoint gravityStep = ccpMult(gravity, dt);
     
@@ -43,18 +43,18 @@
     
     self.velocity = ccpAdd(self.velocity, gravityStep);
     self.velocity = ccp(self.velocity.x * 0.90, self.velocity.y); //2
-   
+    
     
     CGPoint jumpForce = ccp(0.0, 1000.0);
- 
-   
+    
+    
     if (self.mightAsWellJump && self.onGround) {
         
         self.velocity = ccpAdd(self.velocity, jumpForce);
         self.mightAsWellJump = NO;
     }
     
-        self.velocity = ccpAdd(self.velocity, forwardStep);
+    self.velocity = ccpAdd(self.velocity, forwardStep);
     
     CGPoint minMovement = ccp(0.0, -450.0);
     CGPoint maxMovement = ccp(300.0 - (self.enemiesKilled * 50), 300.0);
