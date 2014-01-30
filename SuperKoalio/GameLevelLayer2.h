066592c8106ddd04fcaf85a874982ff77a71621e
ennisthemennis <mennis@mit.edu>
2014-01-29 23:41:57 -0500

    Update: currently just has tutorial levels. Fixed iphone5 screen size problem, swipe up control center problem.

---
+++ SuperKoalio/GameLevelLayer2.h
@@ -0,0 +1,13 @@
+//
+//  GameLevelLayer2.h
+//  Perception
+//
+//  Created by Michaela Ennis on 1/29/14.
+//  Copyright (c) 2014 Interrobang Software LLC. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface GameLevelLayer2 : CCLayer
+
+@end
---
+++ SuperKoalio/GameLevelLayer2.m
@@ -0,0 +1,13 @@
+//
+//  GameLevelLayer2.m
+//  Perception
+//
+//  Created by Michaela Ennis on 1/29/14.
+//  Copyright (c) 2014 Interrobang Software LLC. All rights reserved.
+//
+
+#import "GameLevelLayer2.h"
+
+@implementation GameLevelLayer2
+
+@end
